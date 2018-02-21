73e095dd8b033d22f6260420bd267cbf235cf783
Karl Denby <karldenby@Karls-MacBook-Pro.local>
2018-02-21 16:51:54 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enter your connection String here.
+            string connnection_string = "";
+            string database_name = "hello";
+            string collection_name = "world";
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Connecting to Mongo");
+
+            var client = new MongoClient(connnection_string);
+            var database = client.GetDatabase(database_name);
+            var collection = database.GetCollection<BsonDocument>(collection_name);
+
+            var cursor = collection.Find(new BsonDocument()).ToCursor();
+
+            foreach (var document in cursor.ToEnumerable())
+            {
+                Console.WriteLine(document.ToString());
+            }
+            
+            Console.WriteLine("Done");
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# csharp-hello-world
+C# Experiments
+
+1. Hello World [DONE]
+2. Connect to Mongo Atlas [DONE]
+3. ?
+4. Profit
