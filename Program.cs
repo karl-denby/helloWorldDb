6dab40228f1625ad472e04b295146e369cd3dbb2
karl-denby <karl.denby@gmail.com>
2018-06-13 10:55:20 +0100

    Delete before writing in case of previous execution failure.

--- Program.cs
+++ Program.cs
@@ -12,33 +12,35 @@ namespace myApp
         static void Main(string[] args)
         {
             // Enter your connection String here.
-            string connnection_string = "mongodb+srv://csharp:csharp18@m0-playground-ay8ei.mongodb.net/test?retryWrites=true";
+            string connnection_string = "mongodb+srv://<USER>:<PASSWORD>@dflatretryablewrites-abcde.mongodb.net/test?retryWrites=true";
             string database_name = "hello";
             string collection_name = "world";
 
             Console.WriteLine("Connecting to Mongo");
-
+            
             var client = new MongoClient(connnection_string);
             var database = client.GetDatabase(database_name);
             var collection = database.GetCollection<BsonDocument>(collection_name);
 
-            var cursor = collection.Find(new BsonDocument()).ToCursor();
+            Console.WriteLine("Delete Documents");
+            collection.DeleteMany(new BsonDocument{});
 
-            foreach (var document in cursor.ToEnumerable())
-            {
-                Console.WriteLine(document.ToString());
-            }
-            
+            Console.WriteLine("ServerSelectionTimeoutMS: " + client.Settings.ServerSelectionTimeout);
+
             Console.WriteLine("Insert Documents with 100ms interval");
-            for (int i = 0; i < 10 * 60 * 1; i++)
+            for (int i = 0; i < 10 * 60 * 10; i++)
             {
                 collection.InsertOne(new BsonDocument{
                     {"_id", i},
                     {"desc","Created document _id: " + i.ToString()}
                 });
-                Thread.Sleep(100);
+                Console.Write(i + ", ");
+                if (i % 20 == 0 ){
+                    Console.WriteLine(": Documents Written");
+                }
+                Thread.Sleep(10);
             }
-            Console.WriteLine("Delete 1M Documents");
+            Console.WriteLine("Delete Documents");
             collection.DeleteMany(new BsonDocument{});
             Console.WriteLine("Finished");
         }
